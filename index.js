3638d2f47e67fdaf3df8d4d5fd4d13b81e2eaa55
Aram <arampeltekian2@gmail.com>
2022-09-04 15:20:17 -0500

    commit tree 84895df8fbef2f77d3e7b515874b2725ddc74d57

---
+++ index.js
@@ -0,0 +1,16 @@
+function divide() {
+    return 2000/100;
+  }
+
+
+const square = (parameter1) => parameter1 * parameter1
+multiply(2,2);
+
+
+
+function add(a,b){
+    return a+b
+}
+let add2 = (a , b)=> {
+    return a + b
+}    
